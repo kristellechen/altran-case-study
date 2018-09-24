a6ff3750f4d0744e76d329a24a77819352bd99b8
gilababithutan <teikmenglee@yahoo.com>
2018-09-24 08:30:09 -0400

    Added boilerplate code.

---
+++ readme.txt
@@ -0,0 +1,28 @@
+npm install express
+npm install express-generator -g
+express [name of project]
+npm install
+
+clean up the view-engine.
+delete view directory
+replace res.render with res.send
+delete view-engine setup code in app.js
+npm uninstall jade.
+
+npm install --save-dev nodemon
+modify package.json
+'start' : 'nodemon ./bin/www' 
+
+npm install mongoose
+
+Installing MongoDB
+    
+    Download the MongoDB installer file from the downloads section of the MongoDB website.
+
+    Find the dowloaded .msi file in the Windows Explorer. Double click the file and follow the prompts to install Mongo. Note: unless you specify a custom directory Mongo is most likely installed in the C:\mongodb directory**. However, based on settings on your machine Mongo may be installed other places. For example, C:\Program Files\MongoDB\Server\3.2. Additionally, you may find MongoDB in the add/remove programs menu.
+
+    Create the directory where MongoDB will store it’s files. From the command prompt run md \data\db. This is the default location. However, other locations can be specified using the --dbpath parameter. See the Mongo docs for more information.
+
+    Start the mongodb daemon by running C:\mongodb\bin\mongod.exe in the Command Prompt. Or by running, C:\path\to\mongodb\bin\mongod.exe
+
+    Connect to MongoDB using the Mongo shell While the MongoDB daemon is running, from a different Command prompt window run C:\mongodb\bin\mongo.exe
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,67 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const server = require('../js/server')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('home page');
-});
+router.get('/', function (req, res, next) {
+  res.send('hello')
+})
 
-router.get('/hello', function(req, res, next) {
-  res.send('hello world');
-});
+// Get all studies
+router.get('/studies', function (req, res, next) {
+  res.send('list of studies')
+})
 
-module.exports = router;
+// Get a study by id.
+// localhost:3000/studies/1000
+router.get('/studies/:id', function (req, res, next) {
+  res.send(`Get study with id = ${req.params.id}`)
+})
+
+// Update a study.
+// localhost:3000/studies/1000
+router.put('/studies/:id', function (req, res) {
+  res.send(`Update study with id = ${req.params.id}`)
+})
+
+// Create a study.
+// localhost:3000/studies
+router.post('/studies', function (req, res) {
+  var body = JSON.stringify(req.body)
+  res.send(`Create a new study with id = ${body}`)
+})
+
+// Delete a study.
+// localhost:3000/studies/1000
+router.delete('/studies/:id', function (req, res) {
+  res.send(`Delete study with id = ${req.params.id}`)
+})
+
+// Get Engagement Table
+router.get('/engagement-table', function (req, res, next) {
+  server.getEngagementList().then(resp => {
+    res.json(resp)
+  }).catch(err => {
+    res.json(err)
+  })
+})
+
+// Get Services Table
+router.get('/services-table', function (req, res, next) {
+  server.getServicesList().then(resp => {
+    res.json(resp)
+  }).catch(err => {
+    res.json(err)
+  })
+})
+
+// Get Services Table
+router.get('/keyword-table', function (req, res, next) {
+  server.getKeywordList().then(resp => {
+    res.json(resp)
+  }).catch(err => {
+    res.json(err)
+  })
+})
+
+module.exports = router
