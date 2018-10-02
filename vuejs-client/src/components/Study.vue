0bf42daffc2bd77c53c48c20da0a3661f4a257a0
gilababithutan <teikmenglee@yahoo.com>
2018-10-02 09:57:40 -0400

    used multiline textarea and fixed vertical alignment

--- vuejs-client/src/components/Study.vue
+++ vuejs-client/src/components/Study.vue
@@ -1,96 +1,96 @@
 <template>
   <div>
     <form>
-    <div class='row'>
-        <div class='col-1 labelDiv'>Client</div>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Client</div>
         <div class='col-5'>
           <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client' v-model='study.client'>
         </div>
-        <div class='col-1 labelDiv'>Engagement</div>
+        <div class='col-1 my-auto labelDiv'>Engagement</div>
         <div class='col-5'>
           <Multiselect v-model='selectedEngagementTypes' :options='engagementTypes' :multiple='true'></Multiselect>
         </div>
-    </div>
-    <div class='row'>
-      <div class='col-1 labelDiv'>Project</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.projectName'>
       </div>
-      <div class='col-1 labelDiv'>Discipline</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.discipline'>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Project</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.projectName'>
+        </div>
+        <div class='col-1 my-auto labelDiv'>Discipline</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.discipline'>
+        </div>
       </div>
-    </div>
-    <div class='row'>
-      <div class='col-1 labelDiv'>Year</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.year'>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Year</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.year'>
+        </div>
+        <div class='col-1 my-auto labelDiv'>Summary</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.projectSummary'>
+        </div>
       </div>
-      <div class='col-1 labelDiv'>Summary</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.projectSummary'>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Brand</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.brand'>
+        </div>
+        <div class='col-1 my-auto labelDiv'>Project Team</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.projectTeam'>
+        </div>
       </div>
-    </div>
-    <div class='row'>
-      <div class='col-1 labelDiv'>Brand</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.brand'>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Industry</div>
+        <div class='col-5'><input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.industry'>
+        </div>
+        <div class='col-1 my-auto labelDiv'>Keywords</div>
+        <div class='col-5'>
+          <Multiselect v-model='selectedKeywords' :options='keywords' :multiple='true'></Multiselect>
+        </div>
       </div>
-      <div class='col-1 labelDiv'>Project Team</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.projectTeam'>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Sector</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.sector'>
+        </div>
+        <div class='col-1 my-auto labelDiv'>Project Type</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.projectApplicationType'>
+        </div>
       </div>
-    </div>
-    <div class='row'>
-      <div class='col-1 labelDiv'>Industry</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.industry'>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Services</div>
+        <div class='col-5'>
+          <Multiselect v-model='selectedServices' :options='services' :multiple='true'></Multiselect>
+        </div>
+        <div class='col-1 my-auto labelDiv'>Image</div>
+        <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
+            v-model='study.images'>
+        </div>
       </div>
-      <div class='col-1 labelDiv'>Keywords</div>
-      <div class='col-5'>
-        <Multiselect v-model='selectedKeywords' :options='keywords' :multiple='true'></Multiselect>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Challenges</div>
+        <div class='col-5'>
+          <b-form-textarea class="form-control" rows="5" v-model='study.challenges'></b-form-textarea>
+        </div>
+        <div class='col-1 my-auto labelDiv'>Situation</div>
+        <div class='col-5'>
+          <b-form-textarea class="form-control" rows="5" v-model='study.situation'></b-form-textarea>
+        </div>
       </div>
-    </div>
-    <div class='row'>
-      <div class='col-1 labelDiv'>Sector</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.sector'>
+      <div class='row'>
+        <div class='col-1 my-auto labelDiv'>Results</div>
+        <div class='col-5'>
+          <b-form-textarea class="form-control" rows="5" v-model='study.provenResults'></b-form-textarea>
+        </div>
+        <div class='col-1 my-auto labelDiv'>Solution</div>
+        <div class='col-5'>
+          <b-form-textarea class="form-control" rows="5" v-model='study.solution'></b-form-textarea>
+        </div>
       </div>
-      <div class='col-1 labelDiv'>Situation</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.situation'>
-      </div>
-    </div>
-    <div class='row'>
-      <div class='col-1 labelDiv'>Services</div>
-      <div class='col-5'>
-        <Multiselect v-model='selectedServices' :options='services' :multiple='true'></Multiselect>
-      </div>
-      <div class='col-1 labelDiv'>Solution</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.solution'>
-      </div>
-    </div>
-    <div class='row'>
-      <div class='col-1 labelDiv'>Challenges</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.challenges'>
-      </div>
-      <div class='col-1 labelDiv'>Project Type</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.projectApplicationType'>
-      </div>
-    </div>
-    <div class='row'>
-      <div class='col-1 labelDiv'>Results</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.provenResults'>
-      </div>
-      <div class='col-1 labelDiv'>Image</div>
-      <div class='col-5'> <input type='text' id='inlineFormInputGroup' class='form-control' placeholder='Client'
-          v-model='study.images'>
-      </div>
-    </div>
     </form>
   </div>
 </template>
@@ -147,6 +147,5 @@
 <style scoped>
   .labelDiv {
     text-align: right;
-    line-height: 40px;
   }
 </style>
